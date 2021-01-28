5109b0fa4a620d09f3d3aca9ca55c69341c73c51
libg <664120843@qq.com>
2021-01-28 17:45:15 +0800

    no message

--- modules/mp/src/main/java/com/berg/mp/controller/UserController.java
+++ modules/mp/src/main/java/com/berg/mp/controller/UserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.constraints.NotBlank;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/user")
 @Api(tags = "微信公众号用户")
 public class UserController extends AbstractController {
 
